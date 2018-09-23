bb35bd4a0c3c6f3297fb2d45880621fd59dc707f
Cara Morelli <caramorelli7@gmail.com>
2018-09-23 04:52:28 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,5 +3,7 @@ def oxford_comma(array)
   when array.length == 1 
     array.first 
   when array.length == 2 
-    array.first + ' and ' + array.last
+    array.first + ' and ' + array.last 
+  else 
+  end 
 end
