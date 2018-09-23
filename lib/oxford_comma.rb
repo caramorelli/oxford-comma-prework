959adb906b178d5d839505e64270f752eaaf38a3
Cara Morelli <caramorelli7@gmail.com>
2018-09-23 05:00:56 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,6 +6,6 @@ def oxford_comma(array)
     array.first + ' and ' + array.last 
   else 
     list = array[0..-2].join(', ')
-    list + ' and ' + array[-1]
+    list + ', and ' + array[-1]
   end 
 end
