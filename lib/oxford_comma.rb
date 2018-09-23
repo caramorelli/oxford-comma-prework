d9011f62cbd643e2e1a9f2acd3ebf0c811e081e1
Cara Morelli <caramorelli7@gmail.com>
2018-09-23 04:58:17 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
   when array.length == 2 
     puts array.first + ' and ' + array.last 
   else 
-    array[0..-2].join(', ')
+    list = array[0..-2].join(', ')
     array.insert(-2, 'and ')
   end 
 end
