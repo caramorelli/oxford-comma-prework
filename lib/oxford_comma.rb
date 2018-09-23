afb50653647ce33ab381aa4b298fcc7d4ebe8a3b
Cara Morelli <caramorelli7@gmail.com>
2018-09-23 04:53:04 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,12 @@
 def oxford_comma(array)
   case array.length 
   when array.length == 1 
-    array.first 
+    puts array.first 
   when array.length == 2 
-    array.first + ' and ' + array.last 
+    puts array.first + ' and ' + array.last 
   else 
     array.each_with_index do |ele, index|
+      
     end 
   end 
 end
