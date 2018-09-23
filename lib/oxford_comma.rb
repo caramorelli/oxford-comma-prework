ade718e746a5d07afde5c9ed20f61537182d498e
Cara Morelli <caramorelli7@gmail.com>
2018-09-23 04:52:20 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,5 +2,6 @@ def oxford_comma(array)
   case array.length 
   when array.length == 1 
     array.first 
-  when array.length == 2
+  when array.length == 2 
+    array.first + ' and ' + array.last
 end
